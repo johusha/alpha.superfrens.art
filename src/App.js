b48ace4f390a869f1b24896f9fae11f925ffff34
Travis Worthing <worthingtravis@gmail.com>
2021-11-12 09:07:46 -0800

    added connection to web3modal

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import './App.css'
-import React from 'react'
+import "./App.css";
+import React, { useEffect } from "react";
 
-import StageDoor0 from './components/stages/StageDoor0'
-import StageRoom0 from './components/stages/StageRoom0'
+import StageDoor0 from "./components/stages/StageDoor0";
+import StageRoom0 from "./components/stages/StageRoom0";
+import { useWeb3 } from "./useWeb3";
 
 function App() {
+  const [web3, loading, walletAddress, nftPrice, how_many_nfts, saleStarted] =
+    useWeb3();
+
+  useEffect(() => {
+    console.log(
+      loading,
+      walletAddress,
+      nftPrice,
+      how_many_nfts,
+      saleStarted
+    );
+  }, [web3, loading, walletAddress, nftPrice, how_many_nfts, saleStarted]);
+
   return (
     <>
-      <StageDoor0/>
+      <StageDoor0 />
       {/* <StageRoom0/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
